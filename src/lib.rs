f58cf6463d251bec127bbf78c1994c091e469aac
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-14 20:40:27 +0200

    implement wait_for_element_visible

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use headless_chrome::{protocol::cdp::Page, Browser, LaunchOptionsBuilder, Tab};
+use std::error::Error;
 use std::fs::write;
 use std::sync::Arc;
 
@@ -24,7 +25,7 @@ pub struct ExampleBrowser {
 }
 
 impl ExampleBrowser {
-    pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
+    pub fn new() -> Result<Self, Box<dyn Error>> {
         let browser = Browser::new(LaunchOptionsBuilder::default().headless(true).build()?)?;
 
         let tab = browser.wait_for_initial_tab()?;
@@ -32,21 +33,21 @@ impl ExampleBrowser {
         Ok(Self { browser, tab })
     }
 
-    pub fn navigate_to(&self, page: &ExamplePage) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn navigate_to(&self, page: &ExamplePage) -> Result<(), Box<dyn Error>> {
         self.tab.navigate_to(page.url())?;
         self.tab.wait_until_navigated()?;
         Ok(())
     }
 
-    pub fn get_title(&self) -> Result<String, Box<dyn std::error::Error>> {
+    pub fn get_title(&self) -> Result<String, Box<dyn Error>> {
         self.tab.get_title().map_err(|e| e.into())
     }
 
-    pub fn get_url(&self) -> Result<String, Box<dyn std::error::Error>> {
+    pub fn get_url(&self) -> Result<String, Box<dyn Error>> {
         Ok(self.tab.get_url())
     }
 
-    pub fn get_html(&self) -> Result<String, Box<dyn std::error::Error>> {
+    pub fn get_html(&self) -> Result<String, Box<dyn Error>> {
         self.tab.get_content().map_err(|e| e.into())
     }
 
@@ -63,7 +64,10 @@ impl ExampleBrowser {
         Ok(())
     }
 
-    pub fn fill_form(&self, form_data: &[(String, String)]) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn fill_form(
+        &self,
+        form_data: &[(String, String)],
+    ) -> Result<(), Box<dyn std::error::Error>> {
         let form = self.tab.wait_for_element("form")?;
 
         for (name, value) in form_data {
@@ -76,9 +80,40 @@ impl ExampleBrowser {
     }
 
     pub fn click_link(&self, text: &str) -> Result<(), Box<dyn std::error::Error>> {
-        let link = self.tab.wait_for_element(&format!("a:contains('{}')", text))?;
+        let link = self
+            .tab
+            .wait_for_element(&format!("a:contains('{}')", text))?;
         link.click()?;
         self.tab.wait_until_navigated()?;
         Ok(())
+    }
+
+    pub fn wait_for_element_visible(
+        &self,
+        selector: &str,
+        timeout: u64,
+    ) -> Result<bool, Box<dyn std::error::Error>> {
+        let start_time = std::time::Instant::now();
+
+        loop {
+            match self.tab.wait_for_element(selector) {
+                Ok(element) => {
+                    let box_model = element.get_box_model()?;
+                    if box_model.width == 0.0 || box_model.height == 0.0 {
+                        return Ok(false);
+                    }
+                }
+
+                _ => {
+                    if start_time.elapsed().as_secs() > timeout {
+                        {
+                            return Err("Timeout waiting for element to become visible".into());
+                        }
+                    }
+                }
+            }
+
+            std::thread::sleep(std::time::Duration::from_millis(100));
+        }
     }
 }
