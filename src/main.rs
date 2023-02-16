c0b28103cfd280534491d2cf126c6102959afc65
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-16 21:29:37 +0200

    more readable version

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ use csv::Writer;
 use headless_chrome::LaunchOptionsBuilder;
 use headless_chrome::{Browser, Tab};
 use scraper::{Html, Selector};
+use serde::ser::SerializeStruct;
 use serde::Serialize;
 use std::collections::HashMap;
 use std::error::Error;
 use std::sync::Arc;
 use std::time::Instant;
 
-#[derive(Serialize, Debug)]
+#[derive(Debug)]
 struct Review {
     title: String,
     text: String,
@@ -17,12 +18,50 @@ struct Review {
 }
 
 impl Review {
-    fn from_map(map: HashMap<&str, String>) -> Review {
+    fn from_map(map: HashMap<FieldType, String>) -> Review {
         Review {
-            title: map.get("title").unwrap().to_owned(),
-            text: map.get("text").unwrap().to_owned(),
-            date: map.get("date").unwrap().to_owned(),
-            name: map.get("name").unwrap().to_owned(),
+            title: map[&FieldType::Title].to_owned(),
+            text: map[&FieldType::Text].to_owned(),
+            date: map[&FieldType::Date].to_owned(),
+            name: map[&FieldType::Name].to_owned(),
+        }
+    }
+}
+
+impl Serialize for Review {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut state = serializer.serialize_struct("Review", 4)?;
+        state.serialize_field("title", &self.title)?;
+        state.serialize_field("text", &self.text)?;
+        state.serialize_field("date", &self.date)?;
+        state.serialize_field("name", &self.name)?;
+        state.end()
+    }
+}
+
+#[derive(Copy, Clone, Hash, Eq, PartialEq)]
+enum FieldType {
+    Title,
+    Text,
+    Date,
+    Name,
+}
+
+trait ToFieldType {
+    fn to_field_type(&self) -> FieldType;
+}
+
+impl ToFieldType for &str {
+    fn to_field_type(&self) -> FieldType {
+        match *self {
+            "title" => FieldType::Title,
+            "text" => FieldType::Text,
+            "date" => FieldType::Date,
+            "name" => FieldType::Name,
+            _ => panic!("Invalid field type"),
         }
     }
 }
@@ -30,125 +69,136 @@ impl Review {
 #[derive(Debug)]
 enum FindError {
     ElementNotFound,
-    ClickError,
+}
+
+struct Field {
+    selector: Selector,
+    name: FieldType,
+}
+
+impl Field {
+    fn new(selector: Selector, name: FieldType) -> Field {
+        Field { selector, name }
+    }
+
+    fn extract_value(&self, parent: scraper::ElementRef<'_>) -> Result<String, FindError> {
+        let child = parent
+            .select(&self.selector)
+            .next()
+            .ok_or_else(|| FindError::ElementNotFound)?;
+
+        Ok(child.text().collect::<Vec<_>>().join(", "))
+    }
 }
 
 impl std::fmt::Display for FindError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             FindError::ElementNotFound => write!(f, "Element not found"),
-            FindError::ClickError => write!(f, "Error while clicking on element"),
         }
     }
 }
 
 impl Error for FindError {}
 
-fn get_reviews(
-    tab: Arc<Tab>,
-    page_reviews: Option<usize>,
-    max_reviews: Option<usize>,
-) -> Result<Vec<Review>, Box<dyn Error>> {
-    let start_time = Instant::now();
-    let mut reviews = Vec::new();
+trait ExtractReviews {
+    fn extract_reviews(
+        &self,
+        fields: &[Field],
+        page_reviews: Option<usize>,
+        max_reviews: Option<usize>,
+    ) -> Result<Vec<Review>, Box<dyn Error>>;
+}
 
-    let button_css_selector = ".oke-showMore-button-text.oke-button-text";
-    let main_selector = Selector::parse(".oke-w-reviews-list-item").unwrap();
-    let field_selectors = [
-        (
-            Selector::parse(".oke-reviewContent-title.oke-title").unwrap(),
-            "title",
-        ),
-        (
-            Selector::parse(".oke-reviewContent-body.oke-bodyText").unwrap(),
-            "text",
-        ),
-        (Selector::parse(".oke-reviewContent-date").unwrap(), "date"),
-        (Selector::parse(".oke-w-reviewer-name").unwrap(), "name"),
-    ];
+impl ExtractReviews for Arc<Tab> {
+    fn extract_reviews(
+        &self,
+        fields: &[Field],
+        page_reviews: Option<usize>,
+        max_reviews: Option<usize>,
+    ) -> Result<Vec<Review>, Box<dyn Error>> {
+        let start_time = Instant::now();
+        let mut reviews = Vec::new();
 
-    let mut previous_length = 0;
-    let mut page_number = 0;
-    loop {
-        let start_iteration_time = Instant::now();
+        let button_css_selector = ".oke-showMore-button-text.oke-button-text";
+        let main_selector = Selector::parse(".oke-w-reviews-list-item").unwrap();
 
-        let html = tab.get_content()?;
-        let document = Html::parse_document(html.as_str());
-        let parents = document
-            .select(&main_selector)
-            .skip(previous_length)
-            .collect::<Vec<_>>();
-        for parent in parents {
-            let mut element_map = HashMap::new();
-            for (field_selector, field_name) in field_selectors.iter() {
-                let child = parent
-                    .select(&field_selector)
-                    .next()
-                    .ok_or_else(|| FindError::ElementNotFound)?;
-                let value = child.text().collect::<Vec<_>>().join(", ");
-                element_map.insert(*field_name, value);
+        let mut previous_length = 0;
+        let mut page_number = 0;
+
+        loop {
+            let start_iteration_time = Instant::now();
+            // Extract reviews on the page
+            let html = self.get_content()?;
+            let document = Html::parse_document(&html);
+            let review_elements = document.select(&main_selector);
+            for element in review_elements.skip(previous_length) {
+                let mut field_values = HashMap::new();
+
+                for field in fields {
+                    let value = field.extract_value(element)?;
+                    field_values.insert(field.name, value);
+                }
+
+                reviews.push(Review::from_map(field_values));
             }
-            reviews.push(Review::from_map(element_map));
-        }
 
-        if let Some(max_reviews) = max_reviews {
-            if reviews.len() >= max_reviews {
-                return Ok(reviews);
+            let end_iteration_time = Instant::now();
+            let elapsed_iteration_time = end_iteration_time - start_iteration_time;
+            let elapsed_time = start_time.elapsed();
+            let elapsed_iteration_secs = elapsed_iteration_time.as_secs_f64();
+            let elapsed_secs = elapsed_time.as_secs_f64();
+
+            let current_length = reviews.len();
+
+            if current_length != previous_length {
+                println!("No new reviews found in the last iteration");
+                page_number += 1;
             }
-        }
 
-        if let Some(page_reviews) = page_reviews {
-            if previous_length % page_reviews != 0 {
-                break;
+            println!(
+                "Elapsed time for last iteration: {:.3} seconds",
+                elapsed_iteration_secs
+            );
+            println!("Total elapsed time: {:.3} seconds", elapsed_secs);
+            println!("Number of reviews: {}", current_length);
+            println!("Page number: {}", page_number);
+
+            if let Some(max_reviews) = max_reviews {
+                if current_length >= max_reviews {
+                    return Ok(reviews);
+                }
             }
+
+            if let Some(page_reviews) = page_reviews {
+                if previous_length % page_reviews != 0 {
+                    break;
+                }
+            }
+
+            previous_length = current_length;
+
+            match self.wait_for_element(button_css_selector) {
+                Ok(element) => {
+                    element.click()?;
+                    self.wait_until_navigated()?;
+                }
+                Err(_) => break,
+            };
         }
 
-        let end_iteration_time = Instant::now();
-        let elapsed_iteration_time = end_iteration_time - start_iteration_time;
-        let elapsed_time = start_time.elapsed();
-        let elapsed_iteration_secs = elapsed_iteration_time.as_secs_f64();
-        let elapsed_secs = elapsed_time.as_secs_f64();
+        let elapsed = start_time.elapsed().as_secs_f32();
+        println!("Extracted {} reviews in {:.2}s", previous_length, elapsed);
 
-        let current_length = reviews.len();
-
-        if current_length != previous_length {
-            println!("No new reviews found in the last iteration");
-            page_number += 1;
-        }
-
-        println!(
-            "Elapsed time for last iteration: {:.3} seconds",
-            elapsed_iteration_secs
-        );
-        println!("Total elapsed time: {:.3} seconds", elapsed_secs);
-        println!("Number of reviews: {}", current_length);
-        println!("Page number: {}", page_number);
-
-        previous_length = current_length;
-
-        let button_element = tab.wait_for_element(button_css_selector)?;
-
-        let box_model = match button_element.click() {
-            Err(_) => return Err(Box::new(FindError::ClickError)),
-            Ok(el) => el.get_box_model()?,
-        };
-
-        println!(
-            "button_element (width, height) ({}, {})",
-            box_model.width, box_model.height
-        );
+        Ok(reviews)
     }
-
-    Ok(reviews)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let browser = Browser::new(LaunchOptionsBuilder::default().headless(true).build()?)
-        .map_err(|e| format!("Failed to launch browser: {}", e))?;
+    let launch_options = LaunchOptionsBuilder::default().headless(true).build()?;
+    let browser = Browser::new(launch_options)?;
 
-    let tab = browser
-        .wait_for_initial_tab()
-        .map_err(|e| format!("Failed to get initial tab: {}", e))?;
+    let tab = browser.wait_for_initial_tab()?;
 
     let url = "https://www.tidalhair.com/products/tidal-10-minute-hair-mask?variant=39661967605927";
 
@@ -160,27 +210,35 @@ fn main() -> Result<(), Box<dyn Error>> {
         eprintln!("Failed while waiting for navigation: {}", e);
     }
 
-    let start_time = Instant::now();
+    let fields = [
+        Field::new(
+            Selector::parse(".oke-reviewContent-title.oke-title").unwrap(),
+            FieldType::Title,
+        ),
+        Field::new(
+            Selector::parse(".oke-reviewContent-body.oke-bodyText").unwrap(),
+            FieldType::Text,
+        ),
+        Field::new(
+            Selector::parse(".oke-reviewContent-date").unwrap(),
+            FieldType::Date,
+        ),
+        Field::new(
+            Selector::parse(".oke-w-reviewer-name").unwrap(),
+            FieldType::Name,
+        ),
+    ];
 
-    let reviews = get_reviews(tab, None, None)?;
+    let reviews = Arc::new(tab).extract_reviews(&fields, Some(5), Some(20))?;
 
-    let end_time = Instant::now();
-    let elapsed_time = end_time - start_time;
-    let elapsed_secs = elapsed_time.as_secs_f64();
+    let mut writer = Writer::from_path("reviews.csv")?;
+    writer.write_record(&["Title", "Text", "Date", "Name"])?;
 
-    println!(
-        "Retrieved {} reviews in {:.3} seconds",
-        reviews.len(),
-        elapsed_secs
-    );
-
-    let mut csv_writer = Writer::from_path("reviews.csv")?;
-
-    for review in &reviews {
-        csv_writer.serialize(review)?;
+    for review in reviews {
+        writer.serialize(review)?;
     }
 
-    csv_writer.flush()?;
+    writer.flush()?;
 
     Ok(())
 }
