5dd8c09bf873aed1ba6dbaff2bdfa1b8f6104b19
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-16 14:11:09 +0200

    initial_implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,129 +1,129 @@
-use headless_chrome::{protocol::cdp::Page, Browser, LaunchOptionsBuilder, Tab};
-use std::error::Error;
-use std::fs::write;
-use std::sync::Arc;
+// use headless_chrome::{protocol::cdp::Page, Browser, LaunchOptionsBuilder, Tab};
+// use std::error::Error;
+// use std::fs::write;
+// use std::sync::Arc;
 
-pub enum ExamplePage {
-    HomePage,
-    AboutPage,
-    ContactPage,
-}
+// pub enum ExamplePage {
+//     HomePage,
+//     AboutPage,
+//     ContactPage,
+// }
 
-impl ExamplePage {
-    pub fn url(&self) -> &str {
-        match self {
-            ExamplePage::HomePage => "https://www.example.com",
-            ExamplePage::AboutPage => "https://www.example.com/about",
-            ExamplePage::ContactPage => "https://www.example.com/contact",
-        }
-    }
-}
+// impl ExamplePage {
+//     pub fn url(&self) -> &str {
+//         match self {
+//             ExamplePage::HomePage => "https://www.example.com",
+//             ExamplePage::AboutPage => "https://www.example.com/about",
+//             ExamplePage::ContactPage => "https://www.example.com/contact",
+//         }
+//     }
+// }
 
-pub struct ExampleBrowser {
-    browser: Browser,
-    tab: Arc<Tab>,
-}
+// pub struct ExampleBrowser {
+//     browser: Browser,
+//     tab: Arc<Tab>,
+// }
 
-impl ExampleBrowser {
-    pub fn new() -> Result<Self, Box<dyn Error>> {
-        let browser = Browser::new(LaunchOptionsBuilder::default().headless(true).build()?)?;
+// impl ExampleBrowser {
+//     pub fn new() -> Result<Self, Box<dyn Error>> {
+//         let browser = Browser::new(LaunchOptionsBuilder::default().headless(true).build()?)?;
 
-        let tab = browser.wait_for_initial_tab()?;
+//         let tab = browser.wait_for_initial_tab()?;
 
-        Ok(Self { browser, tab })
-    }
+//         Ok(Self { browser, tab })
+//     }
 
-    pub fn navigate_to(&self, page: &ExamplePage) -> Result<(), Box<dyn Error>> {
-        self.tab.navigate_to(page.url())?;
-        self.tab.wait_until_navigated()?;
-        Ok(())
-    }
+//     pub fn navigate_to(&self, page: &ExamplePage) -> Result<(), Box<dyn Error>> {
+//         self.tab.navigate_to(page.url())?;
+//         self.tab.wait_until_navigated()?;
+//         Ok(())
+//     }
 
-    pub fn get_title(&self) -> Result<String, Box<dyn Error>> {
-        self.tab.get_title().map_err(|e| e.into())
-    }
+//     pub fn get_title(&self) -> Result<String, Box<dyn Error>> {
+//         self.tab.get_title().map_err(|e| e.into())
+//     }
 
-    pub fn get_url(&self) -> Result<String, Box<dyn Error>> {
-        Ok(self.tab.get_url())
-    }
+//     pub fn get_url(&self) -> Result<String, Box<dyn Error>> {
+//         Ok(self.tab.get_url())
+//     }
 
-    pub fn get_html(&self) -> Result<String, Box<dyn Error>> {
-        self.tab.get_content().map_err(|e| e.into())
-    }
+//     pub fn get_html(&self) -> Result<String, Box<dyn Error>> {
+//         self.tab.get_content().map_err(|e| e.into())
+//     }
 
-    pub fn screenshot(&self, path: &str) -> Result<(), Box<dyn std::error::Error>> {
-        let jpeg_data = self.tab.capture_screenshot(
-            Page::CaptureScreenshotFormatOption::Jpeg,
-            None,
-            None,
-            true,
-        )?;
+//     pub fn screenshot(&self, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+//         let jpeg_data = self.tab.capture_screenshot(
+//             Page::CaptureScreenshotFormatOption::Jpeg,
+//             None,
+//             None,
+//             true,
+//         )?;
 
-        write(path, &jpeg_data)?;
+//         write(path, &jpeg_data)?;
 
-        Ok(())
-    }
+//         Ok(())
+//     }
 
-    pub fn fill_form(
-        &self,
-        form_data: &[(String, String)],
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        let form = self.tab.wait_for_element("form")?;
+//     pub fn fill_form(
+//         &self,
+//         form_data: &[(String, String)],
+//     ) -> Result<(), Box<dyn std::error::Error>> {
+//         let form = self.tab.wait_for_element("form")?;
 
-        for (name, value) in form_data {
-            let input = form.find_element(&format!("input[name='{}']", name))?;
-            input.click()?;
-            input.parent.type_str(value)?;
-        }
+//         for (name, value) in form_data {
+//             let input = form.find_element(&format!("input[name='{}']", name))?;
+//             input.click()?;
+//             input.parent.type_str(value)?;
+//         }
 
-        Ok(())
-    }
+//         Ok(())
+//     }
 
-    pub fn click_link(&self, text: &str) -> Result<(), Box<dyn std::error::Error>> {
-        let link = self
-            .tab
-            .wait_for_element(&format!("a:contains('{}')", text))?;
-        link.click()?;
-        self.tab.wait_until_navigated()?;
-        Ok(())
-    }
+//     pub fn click_link(&self, text: &str) -> Result<(), Box<dyn std::error::Error>> {
+//         let link = self
+//             .tab
+//             .wait_for_element(&format!("a:contains('{}')", text))?;
+//         link.click()?;
+//         self.tab.wait_until_navigated()?;
+//         Ok(())
+//     }
 
-    pub fn wait_for_element_visible(
-        &self,
-        selector: &str,
-        timeout: u64,
-    ) -> Result<bool, Box<dyn std::error::Error>> {
-        let start_time = std::time::Instant::now();
+//     pub fn wait_for_element_visible(
+//         &self,
+//         selector: &str,
+//         timeout: u64,
+//     ) -> Result<bool, Box<dyn std::error::Error>> {
+//         let start_time = std::time::Instant::now();
 
-        loop {
-            match self.tab.find_element(selector) {
-                Ok(element) => {
-                    let box_model = element.get_box_model()?;
-                    if box_model.width == 0.0 || box_model.height == 0.0 {
-                        return Ok(false);
-                    }
-                }
+//         loop {
+//             match self.tab.find_element(selector) {
+//                 Ok(element) => {
+//                     let box_model = element.get_box_model()?;
+//                     if box_model.width == 0.0 || box_model.height == 0.0 {
+//                         return Ok(false);
+//                     }
+//                 }
 
-                _ => {
-                    if start_time.elapsed().as_secs() > timeout {
-                        {
-                            return Err("Timeout waiting for element to become visible".into());
-                        }
-                    }
-                }
-            }
+//                 _ => {
+//                     if start_time.elapsed().as_secs() > timeout {
+//                         {
+//                             return Err("Timeout waiting for element to become visible".into());
+//                         }
+//                     }
+//                 }
+//             }
 
-            std::thread::sleep(std::time::Duration::from_millis(100));
-        }
-    }
+//             std::thread::sleep(std::time::Duration::from_millis(100));
+//         }
+//     }
 
-    pub fn reload(&self) -> Result<(), Box<dyn std::error::Error>> {
-        self.tab.reload(true, None)?;
-        self.tab.wait_until_navigated()?;
-        Ok(())
-    }
+//     pub fn reload(&self) -> Result<(), Box<dyn std::error::Error>> {
+//         self.tab.reload(true, None)?;
+//         self.tab.wait_until_navigated()?;
+//         Ok(())
+//     }
 
     
-}
+// }
 
-// implement more advanced methods
+// // implement more advanced methods
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,41 @@
-use headless_scraper::{ExampleBrowser, ExamplePage};
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let browser = ExampleBrowser::new()?;
-    let page = ExamplePage::HomePage;
+use headless_chrome::Browser;
+use headless_chrome::{protocol::cdp::Page, LaunchOptionsBuilder};
+use std::error::Error;
+use std::io::Write;
+fn main() -> Result<(), Box<dyn Error>> {
+    let browser = Browser::new(LaunchOptionsBuilder::default().headless(false).build()?)?;
 
-    println!("Navigating to {}...", page.url());
-    browser.navigate_to(&page)?;
+    let tab = browser.wait_for_initial_tab()?;
 
-    println!("Title: {}", browser.get_title()?);
-    println!("URL: {}", browser.get_url()?);
+    tab.navigate_to(
+        "https://www.tidalhair.com/products/tidal-10-minute-hair-mask?variant=39661967605927",
+    )?;
+
+    tab.wait_until_navigated()?;
+
+    println!("Navigating to {}...", tab.get_url());
+
+    println!("Title: {}", tab.get_title()?);
 
     let html_path = "example.html";
     println!("Saving HTML to {}...", html_path);
-    let html = browser.get_html()?;
+    let html = tab.get_content()?;
     std::fs::write(html_path, html)?;
 
     let screenshot_path = "example.png";
     println!("Taking screenshot and saving to {}...", screenshot_path);
-    browser.screenshot(screenshot_path)?;
-    Ok(())
+    let jpeg_data =
+        tab.capture_screenshot(Page::CaptureScreenshotFormatOption::Jpeg, None, None, true)?;
+
+    let mut file = std::fs::File::create(screenshot_path).unwrap();
+    file.write_all(&jpeg_data).unwrap();
+
+    let button_selector = ".oke-showMore-button-text.oke-button-text";
+
+    loop {
+        let button_element = tab.wait_for_element(button_selector)?;
+        let box_model = button_element.get_box_model()?;
+        println!("width {}, height {}", box_model.width, box_model.height);
+        button_element.click()?;
+    }
 }
